1f44d824df28f6af2cd2eecf85cd8dc7c5ab4ae7
anakint <anakin.t@mail.kmutt.ac.th>
2022-07-08 23:01:53 +0700

    Add ApiSocket

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-const WebSocket = require('ws')
-const wss = new WebSocket.Server({ port: 8080 })
+// Read env variables
+import 'dotenv/config'
+const SERVICE_ACCOUNT_KEY = Buffer.from(process.env.SERVICE_ACCOUNT_KEY, 'base64').toString('utf8')
+const SOCKET_PORT = parseInt(process.env.SOCKET_PORT)
 
-let ping;
-let count = 0;
+// Create server
+import { WebSocketServer } from 'ws'
+const wss = new WebSocketServer({ port: process.env.PORT || SOCKET_PORT })
+console.log('listening at ' + SOCKET_PORT)
 
-wss.on('connection', function connection(ws) {
-  ws.on('close', function close() {
-    console.log('bye')
-    clearInterval(ping);
-    count = 0;
-  })
-  ws.send('hello client')
-  ping = setInterval(() => {
-    ws.send('ping ' + count++)
-  }, 500);
+// Initialize necessary constants
+import { initializeApp, cert } from 'firebase-admin/app'
+import { getFirestore } from 'firebase-admin/firestore'
+import { getAuth } from 'firebase-admin/auth'
+import { nanoid } from 'nanoid'
+import ApiSocket from './src/api-socket.js'
+
+const serviceAccount = JSON.parse(SERVICE_ACCOUNT_KEY)
+initializeApp({
+  credential: cert(serviceAccount),
 })
+const db = getFirestore()
+const auth = getAuth()
+const connectionPool = new Map()
+
+// Bind connection handler
+wss.on('connection', async function connection(ws) {
+  const id = newConnectionId()
+  connectionPool.set(id, new ApiSocket(id, ws, auth, db, () => {
+    connectionPool.delete(id)
+  }))
+})
+
+function newConnectionId() {
+  let id
+  do {
+    id = nanoid()
+  } while (connectionPool.has(id))
+  return id
+}
---
+++ src/api-socket.js
@@ -0,0 +1,160 @@
+import { nanoid } from 'nanoid'
+
+export default class ApiSocket {
+  #id
+  #ws
+  #auth
+  #db
+  #uid
+  #user
+  #subscriptions
+  #onClose
+
+  /***
+   * @param id Connection ID of this client in the socket connection pool
+   * @param ws WebSocket instance of this client
+   * @param auth FirebaseAuth instance
+   * @param db Firestore instance
+   */
+  constructor(id, ws, auth, db, onClose) {
+    this.#id = id
+    this.#ws = ws
+    this.#auth = auth
+    this.#db = db
+    this.#uid = null
+    this.#user = null
+    this.#subscriptions = new Map()
+    this.#onClose = onClose
+    this.#init()
+  }
+
+  #init() {
+    console.log('welcome', this.#id)
+    this.#ws.on('message', (message) => this.#handleMessage(message))
+    this.#ws.on('close', () => this.#close())
+  }
+
+  #verifyToken(token) {
+    this.#auth
+      .verifyIdToken(token)
+      .then(async (decodedToken) => {
+        this.#uid = decodedToken.uid
+        const doc = await this.#db.collection('users').doc(this.#uid).get()
+        this.#user = doc.data()
+        this.#emit('verify-success')
+      })
+      .catch((error) => this.#emit('error', error))
+  }
+
+  #parseMessage(message) {
+    return JSON.parse(Buffer.from(message).toString('utf8'))
+  }
+
+  #handleMessage(message) {
+    const request = this.#parseMessage(message)
+    if (request.type === 'verify') {
+      this.#verifyToken(request.params.token)
+    }
+    
+    if (this.#uid === null) return // Deter further action if not yet verified
+
+    switch (request.type) {
+      case 'subscribe':
+        this.#handleSubscribe(request.params)
+        break
+      case 'unsubscribe':
+        this.#unsubscribe(request.params.sid)
+        break
+      default:
+        break
+    }
+  }
+
+  #newSubscriptionId() {
+    let id
+    do {
+      id = nanoid()
+    } while (this.#subscriptions.has(id))
+    return id
+  }
+
+  #handleSubscribe(params) {
+    const sid = this.#newSubscriptionId()
+    switch (params.collection) {
+      case 'appointments':
+        break
+      case 'chats':
+        this.#subscribeChats(sid)
+        break
+    }
+    this.#emit('subscribe-success', {
+      ref: params.ref,
+      sid: sid,
+    })
+  }
+
+  /***
+   * @example
+   * ```json
+   * {
+   *    "type": "subscribe",
+   *    "params": {
+   *        "collection": "chats",
+   *        "ref": string
+   *    }
+   * }
+   * ```
+   */
+  #subscribeChats(subscriptionId) {
+    const unsubscribe = this.#db
+      .collection('chats')
+      .where(this.#user.user_type, '==', this.#uid)
+      .onSnapshot((querySnapshot) => {
+        console.log('updated')
+        const data = []
+        for (let doc of querySnapshot.docs) {
+          data.push({
+            id: doc.ref.id,
+            ...doc.data(),
+          })
+        }
+        this.#emit('chats', data)
+      })
+    this.#subscriptions.set(subscriptionId, unsubscribe)
+  }
+
+  #emit(event, data) {
+    this.#ws.send(
+      JSON.stringify({
+        event: event,
+        data: data,
+      })
+    )
+  }
+
+  /***
+   * @example
+   * ```json
+   * {
+   *    "type": "unsubscribe",
+   *    "params": {
+   *        "sid": string
+   *    }
+   * }
+   * ```
+   */
+  #unsubscribe(subscriptionId) {
+    if (this.#subscriptions.has(subscriptionId)) {
+      this.#subscriptions.get(subscriptionId)()
+      this.#subscriptions.delete(subscriptionId)
+    }
+  }
+
+  #close() {
+    console.log('bye')
+    for (let [sid, unsubscribe] of this.#subscriptions) {
+      unsubscribe()
+    }
+    if (this.#onClose) this.#onClose()
+  }
+}
